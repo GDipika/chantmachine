4cf219fb04ba5cef0e4e6e0f6fd96138aecc6c22
dipika <guddetidipika11@gmail.com>
2024-02-23 10:41:52 +0530

    added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,48 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'chantmachine';
+  title = 'chantmachine' 
+  count = 0;
+  previousCount = 0;
+  nextCount = 0;
+  isChanting = false;
+  @ViewChild('audioPlayer') audioPlayer: any;
+  chantInterval !: NodeJS.Timeout;
+
+  incrementCount() {
+    this.count++;
+    this.updatePreviousAndNextCounts();
+  }
+
+  decrementCount() {
+    if (this.count > 0) {
+      this.count--;
+      this.updatePreviousAndNextCounts();
+    }
+  }
+
+  startChanting() {
+    this.isChanting = true;
+    this.audioPlayer.nativeElement.play();
+  
+    this.chantInterval = setInterval(() => {
+      this.incrementCount();
+    }, 2000);
+  }
+  stopChanting() {
+    this.isChanting = false;
+    clearInterval(this.chantInterval);
+    this.audioPlayer.nativeElement.pause();
+    this.audioPlayer.nativeElement.currentTime = 0;
+  }
+
+  playAudio() {
+    this.audioPlayer.nativeElement.play();
+  }
+
+  updatePreviousAndNextCounts() {
+    this.previousCount = this.count - 1;
+    this.nextCount = this.count + 1;
+  }
+
 }
